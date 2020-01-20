525f0d90cee814a026765886ca1fa1586355d79e
Niels Krijger <niels@kryger.nl>
2020-01-20 13:48:55 +0100

    chore: update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react-hooks'
+  ],
   extends: [
-    'airbnb',
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    'plugin:@typescript-eslint/recommended' // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   env: {
     browser: true
   },
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true
-    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
     project: './tsconfig.json'
   },
-  plugins: ['react-hooks'],
   rules: {
     'import/prefer-default-export': 0,
     'react/prop-types': 0,
     'react/jsx-one-expression-per-line': 0,
     'react/jsx-props-no-spreading': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
     '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/no-unnecessary-type-assertion': 1,
     '@typescript-eslint/no-unnecessary-qualifier': 1,
@@ -33,16 +33,11 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': 0,
     'react-hooks/rules-of-hooks': 1,
     'react-hooks/exhaustive-deps': 1,
-    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }]
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
   },
   settings: {
     react: {
       version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
     },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-      }
-    }
   }
 };
--- src/components/NL2BR.tsx
+++ src/components/NL2BR.tsx
@@ -8,7 +8,6 @@ const NL2BR: FC<NL2BRProps> = ({ text }): ReactElement => (
   <>
     {text.split('\n').map(
       (item, key): ReactElement => (
-        // eslint-disable-next-line react/no-array-index-key
         <Fragment key={key}>
           {item}
           <br />

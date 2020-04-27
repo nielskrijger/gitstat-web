a71824ab762475f6a247eeb2089340421c1d0b47
Niels Krijger <niels@kryger.nl>
2020-04-27 15:01:04 +0200

    refactor: make storage engine for usePersistedReducer configurable

--- src/hooks/usePersistedReducer.ts
+++ src/hooks/usePersistedReducer.ts
@@ -5,15 +5,16 @@ export function usePersistedReducer<R extends Reducer<any, any>>(
   reducer: R,
   defaultState: any, // Using any here to match reducer API
   key: string,
+  storageEngine: Storage = localStorage,
 ): [ReducerState<R>, Dispatch<ReducerAction<R>>] {
   const [state, dispatch] = useReducer(reducer, defaultState, () => {
-    const stored = localStorage.getItem(key);
+    const stored = storageEngine.getItem(key);
     return stored !== null ? JSON.parse(stored) : defaultState;
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+    storageEngine.setItem(key, JSON.stringify(state));
+  }, [storageEngine, key, state]);
 
   return [state, dispatch];
 }
